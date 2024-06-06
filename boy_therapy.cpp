1b37381ca985da8f32afa943b94b72d8d61f2f39
Kunal Shandilya <kunalshandilyawork@gmail.com>
2024-06-06 16:15:37 +0530

    Update boy_therapy.cpp

--- boy_therapy.cpp
+++ boy_therapy.cpp
@@ -11,16 +11,16 @@
 using namespace std;
 
 bool fileExists(const std::string&filename) {
-    std::ifstream infile(filename);
+    ifstream infile(filename);
     return infile.good();
 }
 
 void writeLog(string whatsup, string whatdo){
-    std::time_t now = std::time(nullptr);
-    std::tm* localTime = std::localtime(&now); // Convert to local time
+    time_t now = std::time(nullptr);
+    tm* localTime = std::localtime(&now); 
     stringstream filename;
     filename << put_time(localTime, "%d-%m-%y-%H-%M-%S") << ".txt";
-    ofstream outfile(filename.str()); // Use the generated filename
+    ofstream outfile(filename.str()); 
     outfile << "whats up: " << whatsup << endl;
     outfile << "what to do: " << whatdo << endl;
     outfile.close();
@@ -36,9 +36,6 @@ int main(){
         string x = "man up "; 
         int n = 69;
         ofstream outfile("boytherapy_model_weights.txt");
-        if (!outfile.is_open()) {
-            return 1;
-        }
         string repeatedX;
         repeatedX.reserve(x.size() * n); // Preallocate for efficiency
         for (int i = 0; i < n; ++i) {
@@ -51,19 +48,19 @@ int main(){
     }
     cout << "Initiating call to Boy... (loading model)" << endl;
     this_thread::sleep_for(chrono::seconds(3));
-    cout << "Boy: sup, all good? (y/n)" << endl;
+    cout << " Boy: sup, all good? (y/n)" << endl;
     string response;
     getline(cin, response);
     if (response == "n"){
-        cout << "Boy: whats up" << endl;
+        cout << " Boy: whats up" << endl;
         string whatsup;
         getline(cin, whatsup);
-        cout << "Boy: damn, thats crazy. so what will you do" << endl;
+        cout << " Boy: damn, thats crazy. so what will you do" << endl;
         string whatdo;
         getline(cin, whatdo);
         writeLog(whatsup, whatdo);
     }
-    cout << "Boy: sounds good. ttyl" << endl;
+    cout << " Boy: sounds good. ttyl" << endl;
     cout << "Boy disconnected. Press any key to exit...";
     getline(cin,response);
     return 0;
